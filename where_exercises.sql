ba58ec26508731f8a0c2536a9e08621f6fbe248f
William Mejia <willx1995@gmail.com>
2022-04-19 15:10:44 -0500

    adding part one of where_exercises.sql

---
+++ where_exercises.sql
@@ -0,0 +1,17 @@
+USE employees;
+
+# Part one
+SELECT *
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+
+SELECT *
+FROM employees
+WHERE last_name LIKE 'e%';
+
+SELECT *
+FROM employees
+WHERE last_name LIKE '%q%';
+
+
+
