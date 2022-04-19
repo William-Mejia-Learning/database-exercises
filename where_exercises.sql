3855485e1af56bdd86d774d53dd386afbb370933
William Mejia <willx1995@gmail.com>
2022-04-19 15:52:27 -0500

    adding part two of where_exercises.sql

--- where_exercises.sql
+++ where_exercises.sql
@@ -13,5 +13,25 @@ SELECT *
 FROM employees
 WHERE last_name LIKE '%q%';
 
+# Part Two
 
+SELECT *
+FROM employees
+WHERE (first_name = 'Irena' AND gender = 'M')
+   OR (first_name = 'Vidya' AND gender = 'M')
+   OR (first_name = 'Maya' AND gender = 'M');
 
+SELECT *
+FROM employees
+WHERE last_name LIKE ('e%')
+   OR last_name LIKE ('%e');
+
+SELECT *
+FROM employees
+WHERE last_name LIKE ('e%')
+   AND last_name LIKE ('%e');
+
+SELECT *
+FROM employees
+WHERE last_name LIKE ('%q%')
+  AND last_name NOT LIKE ('%qu%');
