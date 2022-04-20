dc0bda99af3c6030dc5ef60f43f0d3e022f7792d
William Mejia <willx1995@gmail.com>
2022-04-20 13:56:37 -0500

    adding group_by_exercises.sql to repository

---
+++ group_by_exercises.sql
@@ -0,0 +1,36 @@
+# USE title;
+USE employees;
+
+SELECT DISTINCT title
+FROM titles;
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE ('e%')
+  AND last_name LIKE ('%e')
+GROUP BY last_name
+ORDER BY last_name;
+
+SELECT COUNT(*)
+FROM employees
+WHERE last_name LIKE ('e%')
+  AND last_name LIKE ('%e')
+GROUP BY first_name, last_name;
+
+SELECT DISTINCT last_name
+FROM employees
+WHERE last_name LIKE ('%q%')
+  AND last_name NOT LIKE ('%qu%')
+GROUP BY last_name
+ORDER BY last_name;
+
+SELECT COUNT(last_name), last_name
+FROM employees
+WHERE last_name LIKE ('%q%')
+  AND last_name NOT LIKE ('%qu%')
+GROUP BY last_name;
+
+SELECT COUNT(gender), gender
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+GROUP BY gender;
