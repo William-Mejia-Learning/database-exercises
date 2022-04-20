ebc4ee1e9b7d103cc94d31585a719f98377c02d2
William Mejia <willx1995@gmail.com>
2022-04-20 10:59:27 -0500

    making some changes to the function_exercises.sql file

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -4,12 +4,12 @@ SELECT CONCAT(first_name, ' ', last_name)
 FROM employees
 WHERE first_name LIKE ('%e%');
 
-SELECT *
+SELECT count(*)
 FROM employees
 WHERE month(birth_date) = 12
   AND day(birth_date) = 25;
 
-SELECT *
+SELECT count(*)
 FROM employees
 WHERE month(birth_date) = 12
   AND day(birth_date) = 25
@@ -20,9 +20,11 @@ FROM employees
 WHERE month(birth_date) = 12
   AND day(birth_date) = 25
   AND year(hire_date) BETWEEN 1990 AND 1999
-ORDER BY hire_date DESC;
+ORDER BY birth_date, hire_date DESC
+LIMIT 1;
 
-SELECT NOW(), first_name, last_name, birth_date, hire_date
+SELECT CONCAT('Days at company ', datediff(now(), hire_date)),
+       concat(first_name, ' ', last_name)
 FROM employees
 WHERE month(birth_date) = 12
   AND day(birth_date) = 25
